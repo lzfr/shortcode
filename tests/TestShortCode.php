5b33f32f1a02708856264c4e0b56988d7a5d7054
zhuoliu <lzbrest@yahoo.fr>
2024-07-06 16:31:30 +0800

    add method

--- tests/TestShortCode.php
+++ tests/TestShortCode.php
@@ -4,7 +4,10 @@ namespace Lzfr\Shortcode\Test;
 use Lzfr\Shortcode\ShortcodeGenerator;
 use PHPUnit\Framework\TestCase;
 
- class TestShortCode extends TestCase {
+/**
+ * @test
+ */
+class TestShortCode extends TestCase {
 
      /**
       * @test
@@ -14,5 +17,24 @@ use PHPUnit\Framework\TestCase;
         $generator = new ShortcodeGenerator();
         $code = $generator->generateShortcode();
         $this->assertEquals(6, strlen($code));
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function testLength(){
+        $generator = new ShortcodeGenerator();
+        $code = $generator->generateShortCodeByLength(10);
+        $this->assertEquals(10, strlen($code));
+    }
+
+    public function testPrefix(){
+
+        $generator = new ShortcodeGenerator();
+        $code = $generator->generateShortCodeWithPrefix( 'prefix', 10);
+
+        $this->assertEquals(16, strlen($code));
+        $this->assertEquals('prefix', substr($code, 0, 6));
     }
 }
