8af1bc2e3f089f4b9dda01ae923ccd42364c9b34
zhuoliu <lzbrest@yahoo.fr>
2024-07-06 12:22:16 +0800

    add method

--- src/ShortcodeGenerator.php
+++ src/ShortcodeGenerator.php
@@ -11,4 +11,13 @@ class ShortcodeGenerator
         // Implement your shortcode generation logic here
         return substr(md5(uniqid()), 0, 6); // Example generation logic
     }
+
+    /**
+     * @param $length
+     * @return string
+     */
+    public function generateShortCodeByLength($length) :String
+    {
+        return substr(md5(uniqid()), 0, $length); // Example generation logic
+    }
 }
